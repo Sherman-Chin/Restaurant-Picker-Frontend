0b4a3bbc3e4d621a1b386bd289fcc22b9951906d
Sherman <shermanchin33@gmail.com>
2020-08-23 16:52:49 +1200

    Create select box for cuisines, categories, and establishments

--- src/components/selectionArea/Card.jsx
+++ src/components/selectionArea/Card.jsx
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import Select from "react-select";
+
 function Card(props) {
+    const [data, setData] = useState([]);
+    var newData = [];
+    async function getData() {
+        const response = await axios.get("http://localhost:5000/" + props.listName, {crossdomain: true});
+        return response.data;
+    }
+
+    if (data === undefined || data.length === 0) {
+        getData().then(data => setData(data));
+    } else {
+        if (props.listName === "cuisines") {
+            newData = data.map(({cuisine: {cuisine_name: value,...rest}}) => ({
+                value, 
+                label: value,
+                ...rest
+            }));
+        } else if (props.listName === "establishments") {
+            newData = data.map(({establishment: {name: value,...rest}}) => ({
+                value, 
+                label: value,
+                ...rest
+            }));
+        } else if (props.listName === "categories") {
+            newData = data.map(({categories: {name: value,...rest}}) => ({
+                value, 
+                label: value,
+                ...rest
+            }));
+        }
+    }
+
+
     return (
-        <div>
-            <h2>Hello</h2>
-        </div>
-    );
+        <Select options={newData} />
+        );
 };
 
 export default Card;
--- src/components/selectionArea/SelectionArea.jsx
+++ src/components/selectionArea/SelectionArea.jsx
@@ -1,36 +1,13 @@
 import React, {useState} from "react";
 import Card from "./Card";
-import axios from "axios";
+// import axios from "axios";
 
 function SelectionArea() {
-    var cuisines = [];
-    var establishments = [];
-    var categories = [];
-
-    function getData(param) {
-        axios.get("http://localhost:5000/" + param, {crossdomain: true})
-        .then(response => {
-            if (param === "cuisines") {
-                cuisines.push(response.data);
-            } 
-            else if (param === "establishments") {
-                establishments.push(response.data);
-            } else if (param === "categories") {    
-                categories.push(response.data);
-            } else {
-                console.log("Did not get data");
-            }
-        });
-    }
-    getData("cuisines");
-    getData("establishments");
-    getData("categories");
-
     return (
         <div>
-           <Card list={cuisines} listName="cuisine" />
-           <Card list={establishments} listName="establishment" />
-           <Card list={categories} listName="category" /> 
+            <Card listName="cuisines" />
+            <Card listName= "establishments" />
+            <Card listName="categories" /> 
         </div>
     );
 };
