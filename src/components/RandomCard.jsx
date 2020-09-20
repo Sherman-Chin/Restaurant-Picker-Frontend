230c9ea89b1cbf593922fcf32efc7058049316c5
Sherman <shermanchin33@gmail.com>
2020-09-20 21:33:23 +1200

    Update RandomCard with restaurant information

--- src/components/RandomCard.jsx
+++ src/components/RandomCard.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
+var randomedRestaurant;
 
 function RandomCard(props) {
-    const [restaurants, setRestaurants] = useState([]);
+    const [restaurants, setRestaurants] = useState(undefined);
+    const restaurantGenerated = useRef(false);
+
     const data = {
         "cuisine": props.cuisine,
         "establishment": props.establishment,
@@ -16,18 +19,32 @@ function RandomCard(props) {
             }).catch(err => {
                 console.log(err);
             });
+        restaurantGenerated.current = true;
+    }
+    function randomise() {
+        randomedRestaurant = restaurants[Math.floor(Math.random() * restaurants.length)];
     }
 
-    console.log(restaurants);
+    if (restaurantGenerated.current === true) {
+        randomise();
+        console.log(randomedRestaurant);
+    }
+
+    useEffect(() => {
+        restaurantGenerated.current = false;
+    })
 
     return (
         <div className="card" style={{ width: "18rem" }}>
-            <img src="https://b.zmtcdn.com/data/pictures/3/19428023/c260aad8f4abdd8217525209e6cae49b.jpg" className="card-img-top" alt="..." />
-            <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <button className="btn btn-primary" type="submit" onClick={generateRestaurant}>Go somewhere</button>
-            </div>
+            {randomedRestaurant !== undefined ? <div><img src={randomedRestaurant.restaurant.thumb} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{randomedRestaurant.restaurant.name}</h5>
+                    <p className="card-text">Average cost for two: ${randomedRestaurant.restaurant.average_cost_for_two}</p>
+                    <p className="card-text">Ratings: {randomedRestaurant.restaurant.user_rating.aggregate_rating}</p>
+                    <p className="card-text">Contact number: {randomedRestaurant.restaurant.phone_numbers}</p>
+                    <p className="card-text">Address: {randomedRestaurant.restaurant.location.address}</p>
+                    <button className="btn btn-dark" type="submit" onClick={generateRestaurant}>Get Another Restaurant!</button>
+                </div></div> : <button className="btn btn-dark" type="submit" onClick={generateRestaurant}>Get Restaurant!</button>}
         </div>
     );
 };
